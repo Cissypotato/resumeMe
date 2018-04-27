e241d804276e2e3f977cb2fc1960ad3c20bb68ee
Cissy <1395422644@qq.com>
2018-04-28 01:10:59 +0800

    改了个bug

--- scripts/message.js
+++ scripts/message.js
@@ -55,7 +55,7 @@
                 // })
         },
         bindEvents:function(){
-            this.form.addEventListener('submit',function(e){
+            this.form.addEventListener('submit',(e)=>{
                 e.preventDefault();
                 this.saveMessage()
             })
